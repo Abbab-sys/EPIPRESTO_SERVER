caf16cfa6839a2c7668e97a2299e0b3be5170804
Adam Naoui <adam.naoui@polymtl.ca>
2022-10-23 00:23:43 -0400

    GL4A2209-32: hot fix: removing deleted variant in variantsIds field for product

--- mongodb/ProductsSource.js
+++ mongodb/ProductsSource.js
@@ -4,11 +4,12 @@ import sanitize from 'mongo-sanitize';
 
 export default class ProductsSource extends MongoDataSource {
     async findOneById(id) {
-        id=sanitize(id);
+        id = sanitize(id);
         return await this.collection.findOne({_id: new ObjectId(id)})
     }
+
     async getProductsByIds(productsIds) {
-        productsIds=sanitize(productsIds);
+        productsIds = sanitize(productsIds);
         if (!productsIds || productsIds.length === 0) return []
         return await this.collection.find({
             _id: {"$in": productsIds}
@@ -17,39 +18,42 @@ export default class ProductsSource extends MongoDataSource {
 
     //create product
     async createProduct(product) {
-        const {variants,...productWithoutVariants} = product;
+        const {variants, ...productWithoutVariants} = product;
         productWithoutVariants.variantsIds = [];
         const {insertedId} = await this.collection.insertOne(productWithoutVariants);
         return insertedId;
     }
 
     async updateProductById(idProduct, fieldsToUpdate) {
-        idProduct=sanitize(idProduct);
+        idProduct = sanitize(idProduct);
         const query = {_id: new ObjectId(idProduct)};
         const updateValues = {$set: fieldsToUpdate};
         return await this.collection.updateOne(query, updateValues);
     }
+
     async addNewVariantToProduct(productId, newVariantId) {
-        productId=sanitize(productId);
+        productId = sanitize(productId);
         const query = {_id: new ObjectId(productId)};
         const updateVariants = {$push: {variantsIds: newVariantId}};
         return await this.collection.updateOne(query, updateVariants);
     }
+
     async removeVariantFromProduct(productId, variantId) {
-        productId=sanitize(productId);
+        productId = sanitize(productId);
         const query = {_id: new ObjectId(productId)};
-        const updateVariants = {$pull: {variantsIds: variantId}};
+        const updateVariants = {$pull: {variantsIds: new ObjectId(variantId)}};
         return await this.collection.updateOne(query, updateVariants);
     }
+
     async deleteProductById(idProduct) {
-        idProduct=sanitize(idProduct);
+        idProduct = sanitize(idProduct);
         const query = {_id: new ObjectId(idProduct)};
         return await this.collection.deleteOne(query);
     }
 
     //GET single product by id
     async getProductByShopifyId(idProduct) {
-        idProduct=sanitize(idProduct);
+        idProduct = sanitize(idProduct);
         return await this.collection.findOne({shopifyProductId: idProduct});
     }
 
