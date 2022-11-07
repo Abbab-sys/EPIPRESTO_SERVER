2eab140f78b7c4d18b4b2516ecee92605892eb01
Adam Naoui <adam.naoui@polymtl.ca>
2022-11-07 00:16:37 -0500

    adding mock stripe

--- resolvers/queries-resolvers.js
+++ resolvers/queries-resolvers.js
@@ -3,11 +3,14 @@ import {
 } from "./queries-resolvers/queries-vendors-credentials-checking-resolvers.js";
 import {queriesVendorsLoginResolvers} from "./queries-resolvers/queries-vendors-login-resolvers.js";
 import {queriesGettersByIdResolvers} from "./queries-resolvers/queries-getters-by-id-resolvers.js";
-import { queriesAnalyticsResolvers } from "./queries-resolvers/queries-analytics-resolvers.js";
+import {queriesAnalyticsResolvers} from "./queries-resolvers/queries-analytics-resolvers.js";
 import {queriesClientsLoginResolvers} from "./queries-resolvers/queries-clients-login-resolvers.js";
 import {
     queriesClientsCredentialsCheckingResolvers
 } from "./queries-resolvers/queries-clients-credentials-checking-resolvers.js";
+
+import * as stripePackage from "stripe";
+
 const queriesResolvers = {
     Query: {
         ...queriesVendorsCredentialsCheckingResolvers,
@@ -16,6 +19,33 @@ const queriesResolvers = {
         ...queriesClientsLoginResolvers,
         ...queriesGettersByIdResolvers,
         ...queriesAnalyticsResolvers,
+        getStripe: async () => {
+            const stripe = stripePackage.Stripe('sk_live_bA0XUdrZQUUlbt0CuN4Y7kQp000wUU5OiN');
+            // Use an existing Customer ID if this is a returning customer.
+            const customer = await stripe.customers.create();
+            const ephemeralKey = await stripe.ephemeralKeys.create(
+                {customer: customer.id},
+                {apiVersion: '2022-08-01'}
+            );
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: 1099,
+                currency: 'eur',
+                customer: customer.id,
+                automatic_payment_methods: {
+                    enabled: true,
+                },
+            });
+            return {
+                code: 200,
+                message: "Stripe data retrieved",
+                stripe: {
+                    paymentIntent: paymentIntent.client_secret,
+                    ephemeralKey: ephemeralKey.secret,
+                    customer: customer.id,
+                    publishableKey: "pk_live_wKUFHBGUKYlaCqEAEaJtuHP000dvnnJ0p6"
+                }
+            }
+        }
     },
 };
 export {queriesResolvers}
